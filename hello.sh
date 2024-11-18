9f031ccda55ac13c82b1e3a47517eee21a2a91ee
Vijay <v.sanika357@gmail.com>
2024-11-18 12:27:57 +0530

    merging branch

--- hello.sh
+++ hello.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 
 echo "hello from C-DAC ITIM-Devops " | lolcat
 echo " Hello from developer 1  "
@@ -7,3 +8,7 @@ echo " Hello from developer 1  "
 echo " Hello from C-DAC DITISS" | lolcat
 echo "from devloper 2"
 
+=======
+echo " Hello from C-DAC DITISS" | lolcat
+echo "from devloper 2"
+>>>>>>> bug-2-branch
